eb97c1842dc50232d34deb45cbb21a1dd1a14e82
LeTrongDuc2510 <leducpro25102003@gmail.com>
2025-02-11 08:52:37 +0700

    make load mtl file dynamic and change all Ka, Kd, Ks to 0.5

--- shape/object3D_v2.py
+++ shape/object3D_v2.py
@@ -21,8 +21,8 @@ class Object:
         self.subObjs = []
 
         self.dir_path = os.path.dirname(file_path)
-        mtl_path = file_path.replace(".obj", ".mtl")
-        self.materials = self.load_materials(mtl_path)
+        # mtl_path = file_path.replace(".obj", ".mtl")
+        # self.materials = self.load_materials(mtl_path)
 
         overall_min, overall_max = self.parse_file_pywavefront(file_path)
         self.min_x, self.min_y, self.min_z = overall_min
@@ -31,6 +31,14 @@ class Object:
     def parse_file_pywavefront(self, obj_file):
         scene = pywavefront.Wavefront(obj_file, collect_faces=False)
         print("Finished Parsing PyWavefront")
+
+        if scene.mtllibs:
+            print(f"Material libraries: {scene.mtllibs}")
+            mtl_path =  os.path.join(self.dir_path, scene.mtllibs[0])
+        else:
+            mtl_path = obj_file.replace(".obj", ".mtl")
+        self.materials = self.load_materials(mtl_path)
+
 
         overall_min = np.array([float('inf'), float('inf'), float('inf')])
         overall_max = np.array([float('-inf'), float('-inf'), float('-inf')])
